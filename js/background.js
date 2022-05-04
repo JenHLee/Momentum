19c20b6a218a11ac323e9a50d7ba636a2f1c693f
JenHLee <hj.jennie.lee@gmail.com>
2022-05-03 22:57:56 -0600

    add js

---
+++ js/background.js
@@ -0,0 +1,18 @@
+const images = [
+"0.jpg",
+"1.jpg",
+"2.jpg",
+"3.jpg",
+"4.jpg",
+"5.jpg",
+"6.jpg",
+]
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+const bgImage = document.createElement("img"); // to create Element (same as in the HTML)
+bgImage.src = `img/${chosenImage}`;
+
+console.log(bgImage);
+
+document.body.appendChild(bgImage); //add HTML body
