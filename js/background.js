c5827c2e1550c7b1357c841a96d6e4990d16511c
JenHLee <hj.jennie.lee@gmail.com>
2022-05-17 23:22:37 -0600

    add quote api, weather icon

--- js/background.js
+++ js/background.js
@@ -12,6 +12,6 @@ const chosenImage = images[Math.floor(Math.random() * images.length)];
 const bgImage = document.createElement("img"); // to create Element (same as in the HTML)
 bgImage.src = `img/${chosenImage}`;
 
-console.log("bgimage number: " + chosenImage);
+//console.log("bgimage number: " + chosenImage);
 
 document.body.appendChild(bgImage); //add HTML body
--- js/weather.js
+++ js/weather.js
@@ -9,12 +9,19 @@ function onGeoOk(position) {
     fetch(url)
         .then((response) => response.json())
         .then((data) => {
-        const weather = document.querySelector("#weather span:first-child"); 
-        const city = document.querySelector("#weather span:last-child"); 
-        let temp = Math.round(data.main.temp);
-        weather.innerText = `${data.weather[0].main} | ${temp}⁣C°`;
-        city.innerText = `${data.name}`;
-});
+            const weather = document.querySelector("#weather span:nth-child(2)");
+            const city = document.querySelector("#weather span:last-child");
+            
+            const icon = data.weather[0].icon;
+            const weatherIcon = document.getElementById("icon");
+            weatherIcon.src = `./icons/${icon}.png`;
+            document.body.appendChild(weatherIcon);
+            
+            let weatherTemp = Math.round(data.main.temp);
+            weather.innerText = `${weatherTemp}⁣C°`;
+            city.innerText = `${data.name}`;
+
+        });
 }
 //lat: 51.0492672 lng: -114.0785152
 //https://api.openweathermap.org/data/2.5/weather?lat=51.0492672&lon=-114.0785152&appid=5cb9f996e681a6327baa22d71efa58fe
